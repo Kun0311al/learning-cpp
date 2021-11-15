ae754bb362c9f094411411f3121178338fe09bb4
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-11-15 19:21:50 +0530

    conditional operator

--- operators/conditional_operators.cpp
+++ operators/conditional_operators.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b;
-    
+    int a;
+    cout<<"enter number between 0 to 9: "<<endl;
+    cin>>a;
+    if(a>=0 && a<=9){
+        cout<<"your number is valid..!";
+    }
+    else{
+        cout<<"enter valid number...";
+    }
+    return 0;
 }
