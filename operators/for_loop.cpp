97c5c849567993e44b86eb516f53a51bb3198805
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-11-15 21:42:25 +0530

    for loop syntex

---
+++ operators/for_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter number: "<<endl;
+    cin>>a;
+    cout<<"get your numbers: "<<endl;
+    for(b=1;b<=a;b++){
+        cout<<b<<endl;
+    }
+    return 0;
+}
