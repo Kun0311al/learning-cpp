3f923e81fb40ef42045667e41158092afa9a3cc3
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-11-15 19:05:01 +0530

    basic operators

---
+++ operators/basic_operations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers: "<<endl;
+    cin>>a>>b;
+    cout<<"addition is: "<<a+b<<endl;
+    cout<<"subtraction is: "<<a-b<<endl;
+    cout<<"multiplication is: "<<a*b<<endl;
+    cout<<"division is: "<<a/b<<endl;
+    return 0;
+}
