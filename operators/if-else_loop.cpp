db892dd5147ca347c2e0c8460d40a7a8237e84f8
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-11-15 21:25:50 +0530

    if else statement

--- operators/if-else_loop.cpp
+++ operators/if-else_loop.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main(){
     int a;
-    cout<<""
+    cout<<"enter your age:"<<endl;
+    cin>>a;
+    if(a>=18 && a<=120){
+        cout<<"you are now mature..!"<<endl;
+    }
+    else(a<18){
+        cout<<"you are child..."<<endl;
+    }
+    else(a>120){
+        cout<<"please enter valid age..."<<endl;
+    }
+    return 0;
 }
