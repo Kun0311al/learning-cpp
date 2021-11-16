c7f8b9ddabe023e9185d9995cd9285a97213602e
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-11-16 15:22:55 +0530

    code of nested loop

---
+++ operators and loops/nested_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=3,b=7;
+    for(int c=1;c<=a;c++){
+        cout<<"week: "<<c<<endl;
+        for(int j=1;j<=b;j++){
+            cout<<"   day: "<<j<<endl;
+        }
+    }    
+    return 0;
+}
